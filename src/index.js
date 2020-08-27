7aa768d7945304c1b3e137a67b3e4dba56833cbc
gefilte84 <58055602+gefilte84@users.noreply.github.com>
2020-08-27 13:23:13 +0200

    setState

--- src/index.js
+++ src/index.js
@@ -4,20 +4,25 @@ import ReactDOM from 'react-dom';
 // skaffer posisjonen til bruker. Klasse basert komponent
 
 class App extends React.Component {
-    // initierer 'state'
+    // initierer 'state i app komponent'
     constructor(props) {
         super(props); // må være med når vi definerer en konstruktør innenfor en klasse basert komponent
     
         this.state = { lat: null }; // setter null fordi vi vet ikke posisjon enda
+    
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // vi oppdaterer state med setState
+                this.setState({ lat: position.coords.latitude});
+            },
+            err => console.log(err)
+        
+            );
     }
     // react sier vi MÅ ha en render metode!
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    
-        );
-        return <div> Latitude: </div>
+       
+        return <div> Latitude: {this.state.lat}</div>
     }
 }
 
