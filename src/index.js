f931d8468c55bad0cd30204475237cdb138f7f44
gefilte84 <58055602+gefilte84@users.noreply.github.com>
2020-08-27 14:39:49 +0200

    oppdatering 1.0

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // skaffer posisjonen til bruker. Klasse basert komponent
 
 class App extends React.Component {
     // initierer 'state i app komponent'
-    constructor(props) {
-        super(props); // må være med når vi definerer en konstruktør innenfor en klasse basert komponent
+    //constructor(props) {
+      //  super(props); // må være med når vi definerer en konstruktør innenfor en klasse basert komponent
         
         // Dette er eneste ganger vi setter attributter til this.state
-        this.state = { lat: null, errorMessage: '' }; // setter null fordi vi vet ikke posisjon enda
-    
+        //this.state = { lat: null, errorMessage: '' }; // setter null fordi vi vet ikke posisjon enda
+    // alternative måte å initiere
+    state = { lat: null, errorMessage: ''};
+    componentDidMount (){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
+            position => this.setState({ lat: position.coords.latitude}),
                 // vi oppdaterer position med setState
-                this.setState({ lat: position.coords.latitude});
-            },
-            err => {
+            err => this.setState ({ errorMessage: err.message })
                 // håndterer error når bruker ikke tillater posisjon
-                this.setState ({ errorMessage: err.message })
-            }
-        
+                
             );
+        
     }
-
+    // Lifecycle metoder
+    // dataloading burde alltid gjøres i componentDidMount
+    // componentDidUpdate burde alltid brukes til dataloading når en komponent oppdater. 
+    // input fra bruker eller knappetrykk
+    // componentWillUnmount brukes til å rydde
     
-    // react sier vi MÅ ha en render metode!
+    // react sier vi MÅ ha en render metode! Dette er hva bruker ser.
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />       
         }
         return <div>Loading!</div>;
     }
