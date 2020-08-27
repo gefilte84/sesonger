6093d047cabb5dbd69044393865166001215132f
gefilte84 <58055602+gefilte84@users.noreply.github.com>
2020-08-27 13:14:07 +0200

    initier state

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom';
 // skaffer posisjonen til bruker. Klasse basert komponent
 
 class App extends React.Component {
+    // initierer 'state'
+    constructor(props) {
+        super(props); // må være med når vi definerer en konstruktør innenfor en klasse basert komponent
+    
+        this.state = { lat: null }; // setter null fordi vi vet ikke posisjon enda
+    }
+    // react sier vi MÅ ha en render metode!
     render() {
         window.navigator.geolocation.getCurrentPosition(
         position => console.log(position),
